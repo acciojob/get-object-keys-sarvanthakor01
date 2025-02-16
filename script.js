b503e3f163d0eeac6c3a37b941563647749830e7
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-16 17:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create the student object
+let student = {
+  name: "John"
+};
+
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj); // Returns an array of keys
+}
+
+// Testing the function
+console.log(getKeys(student)); // Output: ["name"]
